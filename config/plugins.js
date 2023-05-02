6036aa2991b98cc2271212a1b0c1f3ea6f1e0264
Aylay <luk.tattali@gmail.com>
2023-05-02 12:12:36 +0200

    notif mail

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,21 @@ module.exports = ({ env }) => ({
     enabled: true,
     resolve: './src/plugins/content-export-import' // path to plugin folder
   },
+  email: {
+    config: {
+      provider: env('EMAIL_PROVIDER'),
+      providerOptions: {
+        host: env('EMAIL_SMTP_HOST', 'smtp.example.com'),
+        port: env('EMAIL_SMTP_PORT', 587),
+        auth: {
+          user: env('EMAIL_SMTP_USER'),
+          pass: env('EMAIL_SMTP_PASS'),
+        }
+      },
+      settings: {
+        defaultFrom: env('EMAIL_ADDRESS_FROM'),
+        defaultReplyTo: env('EMAIL_ADDRESS_REPLY'),
+      },
+    },
+  }
 });
--- src/api/prospect/controllers/prospect.js
+++ src/api/prospect/controllers/prospect.js
@@ -6,4 +6,34 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::prospect.prospect');
+module.exports = createCoreController('api::prospect.prospect', ({ strapi }) => ({
+  async create(ctx) {
+    const response = await super.create(ctx);
+
+    const mailList = 'lattali@beavers-agency.fr'
+    let textMail = 'Bonjour'
+    textMail += '<br /><br />'
+    textMail += "Cette personne a fait une demande d'informations.<br /><br />"
+
+    textMail += 'Ses coordonnées :'
+    textMail += '<br />'
+    textMail += 'Prénom : ' + response.data.attributes.prenom + '<br />'
+    textMail += 'Nom : ' + response.data.attributes.nom + '<br />'
+    textMail += 'Société : ' + response.data.attributes.societe + '<br />'
+    textMail += 'Fonction : ' + response.data.attributes.fonction + '<br />'
+    textMail += 'Email : ' + response.data.attributes.email + '<br />'
+    textMail += 'Téléphone : ' + response.data.attributes.telephone + '<br />'
+    textMail += "Code postal de l'entreprise : " + response.data.attributes.codePostal + '<br />'
+    textMail += '<br />'
+
+    await strapi
+    .plugin('email')
+    .service('email')
+    .send({
+      to: mailList,
+      from: 'ocs@richard.fr',
+      subject: 'Formulaire abonnement',
+      html: textMail,
+    });
+  }
+}))
